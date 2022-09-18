03d7185de54eec082ee766f787baa9da2cb97c24
workwithamit <workwithamit89@gmail.com>
2022-09-19 00:20:27 +0530

    fresh commit

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,12 +1,38 @@
-const { network } = require("hardhat");
+const { network, ethers } = require("hardhat");
+const { developmentChains, networkConfig } = require("../helper-hardhat-config");
+const {verify} = require("../utils/verify");
 
+const FUND_AMOUNT = "1000000000000000000000"
 module.exports = async function({getnamedAccounts, deployments}){
     const {deploy, log} = deployments;
     const {deployer} = await getnamedAccounts;
+    const chainId = network.config.chainId
+
+    let vrfCoordinatorV2Address, subscriptionId;
+
+    if(developmentChains.includes(network.name)){
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address; 
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+        const transactionReceipt = await transactionResponse.wait();
+        subscriptionId = transactionReceipt.events[0].args.subId;
+        //Fund the subscription
+        //our mocks makes it so we don't actually have to worry about sending fund
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId,FUND_AMOUNT);
+
+    }
+    else{ 
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinator"] ;
+        subscriptionId = networkConfig[chainId]["SubscriptionId"]
+    }
+
+    const entranceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"];
+    const args = [vrfCoordinatorV2Address,entranceFee];
     const raffle = await deploy("Raffle",
     {
         from: deployer,
-        args: [],
+        args: args,
         log: true, 
         waitConfirmations: network.config.blockConfirmations || 1,
         
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,8 +1,19 @@
+const { ethers } = require("hardhat")
+
 const networkConfig = {
     4:{
         name:"rinkeby",
         vrfCoordinatorV2:"0x271682DEB8C4E0901D1a1550aD2e64D568E69909",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane:"0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+        subscriptionId : "0",
     },
+    31337:{
+        name:"hardhat",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane:"0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+
+    }
 
 }
 const developmentChains = ["hardhat","localhost"]
