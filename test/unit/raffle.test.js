35b322fbf13f42f03aa41015ab3577a5587964c6
workwithamit <workwithamit89@gmail.com>
2022-09-20 21:51:36 +0530

    unit test continued

--- test/unit/raffle.test.js
+++ test/unit/raffle.test.js
@@ -13,7 +13,7 @@ const{developmentChains, networkConfig} = require("../../helper-hardhat-config")
         vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer);
     })
 
-    describe("constructor", async function(){
+    describe("constructor",  function(){
         it("Initializes the raffle correctly",async function(){
             //Ideally we make our tests have just 1 assert per "it"
             const raffleState = await raffle.getRaffleState();
@@ -24,7 +24,7 @@ const{developmentChains, networkConfig} = require("../../helper-hardhat-config")
         })
     })
 
-    describe("enterRaffle", async function(){
+    describe("enterRaffle",  function(){
         it("reverts when you don't pay enough", async function(){
             await expect(raffle.enterRaffle()).to.be.revertedWith(
                 "Raffle__NotEnoughETHEntered"
@@ -56,21 +56,72 @@ const{developmentChains, networkConfig} = require("../../helper-hardhat-config")
          * so we will use callStatic to get that
          */
 
-        it("doesn't allow entrance when raffle is calculating", async function(){
+         it("doesn't allow entrance when raffle is calculating", async () => {
+            await raffle.enterRaffle({ value: raffleEntranceFee })
+            // for a documentation of the methods below, go here: https://hardhat.org/hardhat-network/reference
+            await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+            await network.provider.request({ method: "evm_mine", params: [] })
+            // we pretend to be a keeper for a second
+            await raffle.performUpkeep([]) // changes the state to calculating for our comparison below
+            await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.be.revertedWith( // is reverted as raffle is calculating
+                "Raffle__RaffleNotOpen"
+            )
+        })
+    })
+    describe("checkUpkeep", function () {
+        //hasBalance
+        it("returns false if people haven't sent any ETH", async () => {
+            await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+            await network.provider.request({ method: "evm_mine", params: [] })
+            const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x") // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+            assert(!upkeepNeeded)
+        })
+        //raffle --> isOpen
+        it("returns false if raffle isn't open", async () => {
+            await raffle.enterRaffle({ value: raffleEntranceFee })
+            await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+            await network.provider.request({ method: "evm_mine", params: [] })
+            await raffle.performUpkeep([]) // changes the state to calculating
+            const raffleState = await raffle.getRaffleState() // stores the new state
+            const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x") // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+            assert.equal(raffleState.toString() == "1", upkeepNeeded == false)
+        })
+        //timePassed
+        it("returns false if enough time hasn't passed", async () => {
+            await raffle.enterRaffle({ value: raffleEntranceFee })
+            await network.provider.send("evm_increaseTime", [interval.toNumber() - 5]) // use a higher number here if this test fails
+            await network.provider.request({ method: "evm_mine", params: [] })
+            const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x") // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+            assert(!upkeepNeeded)
+        })
+        
+        it("returns true if enough time has passed, has players, eth, and is open", async () => {
+            await raffle.enterRaffle({ value: raffleEntranceFee })
+            await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+            await network.provider.request({ method: "evm_mine", params: [] })
+            const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x") // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+            assert(upkeepNeeded)
+        })
+    })
+
+    describe("performUpkeep", function(){
+        it("it can only run if checkupkeep is true", async function(){
             await raffle.enterRaffle({value: raffleEntranceFee});
             await network.provider.send("evm_increaseTime",[interval.toNumber() + 1]);
             await network.provider.send("evm_mine",[]);
-            //we pretend to be a chainlink keeper
-            await raffle.performUpkeep([]);
-            await expect(raffle.enterRaffle({value: raffleEntranceFee})).to.be.revertedWith("Raffle__RaffleNotOpen");
+            const tx = await raffle.performUpkeep([]);
+            assert(tx);
         })
-        describe ("checkUpkeep", async function(){
-            it("returns false if people haven't sent any ETH", async function(){
-                await network.provider.send("evm_increaseTime",[interval.toNumber() + 1]);
-                await network.provider.send("evm_mine",[]);
-                const {upkeepNeeded} = await raffle.callStatic.checkUpkeep([]);
-                assert(!upkeepNeeded);
-            })
+        it("reverts when checkupkeep is false", async function(){
+            await expect(raffle.performUpkeep([])).to.be.revertedWith("Raffle__UpkeepNotNeeded");
+            /**
+             * our test is smart enough to know about the error by the name
+             * that it reverts it with all the extra stuff which is in the error.
+             * Although we can pass the parameter using string interpolation
+             // await expect(raffle.performUpkeep([])).to.be.revertedWith(
+                `Raffle__UpkeepNotNeeded(we can put all the stuff here)`);
+           
+             */ 
         })
     })
 
